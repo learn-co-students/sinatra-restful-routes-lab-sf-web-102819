6bf3daccf0ab45d9463034213619dc55c2d7bcb8
Joseh <joseph161216@gmail.com>
2019-11-19 21:28:14 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +6,53 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get "/" do 
+    redirect to "/recipes"
+  end
+  
+  #index
+  get "/recipes" do 
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  #create
+  get "/recipes/new" do 
+    erb :new
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  #show
+  get "/recipes/:id" do 
+    @recipe = Recipe.find(params[:id])
+    # binding.pry
+    erb :show
+  end
+
+  #edit
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+  
+  patch "/recipes/:id" do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+
+  #delete
+  delete "/recipes/:id" do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,17 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base
+  # attr_accessor :name, :ingredients, :cook_time
+
+  # @@all = []
+
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @ingredients = []
+  #   @cook_time = params[:cook_time]
+  #   @@all << self
+  # end
+
+  # def self.all
+  #   @@all
+  # end
+end
